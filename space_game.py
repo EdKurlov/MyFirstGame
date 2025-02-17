bc7adaf8cc9296eedd01649ee9294683cce278ca
Eduard Kurlov <edgreen.n.b@gmail.com>
2025-02-17 15:35:45 +0200

    Create a screen for the game

---
+++ space_game.py
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+import pygame
+import sys
+
+def run():
+
+    pygame.init()
+    screen = pygame.display.set_mode((1200, 800))
+    pygame.display.set_caption('space defenders')
+    bg_color = (0, 0, 0)
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+
+        screen.fill(bg_color)
+        pygame.display.flip()
+
+run()
+
